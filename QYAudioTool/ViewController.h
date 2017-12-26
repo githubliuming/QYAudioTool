658c4b5a4dca7a4aab86be960b5661d362e7dc0a
liuming <1969489778@qq.com>
2017-12-26 16:02:19 +0800

    穿件demo

---
+++ QYAudioTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYAudioTool
+//
+//  Created by liuming on 2017/12/26.
+//  Copyright © 2017年 yoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QYAudioTool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QYAudioTool
+//
+//  Created by liuming on 2017/12/26.
+//  Copyright © 2017年 yoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
